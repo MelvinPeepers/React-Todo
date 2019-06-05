d3c628f42ac89a903d11180f392227d32e75f399
Melvin Peepers <peepersm@gmail.com>
2019-06-04 22:39:50 -0400

    imported ToDoList

--- src/App.js
+++ src/App.js
@@ -1,42 +1,6 @@
 import React from 'react';
 import TodoList from "./components/TodoComponents/TodoList";
 
-
-const todos = [
-  {
-    task: "Do dishes",
-    id: 1,
-    completed: false
-  },
-  {
-    task: "Make lunches",
-    id: 2,
-    completed: false
-  },
-  {
-    task: "Pack bags",
-    id: 3,
-    completed: false
-  },
-  {
-    task: "Pick out cloths",
-    id: 4,
-    completed: false
-  },
-  {
-    task: "Brush Teeth",
-    id: 5,
-    completed: false
-  },
-  {
-    task: "Go to bed",
-    id: 6,
-    completed: false
-  },
-
-];
-
-
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -44,19 +8,26 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todosOnState: todos
+      todos: [
+        {
+          task: "Do dishes",
+          id: 1,
+          completed: false
+        },
+        {
+          task: "Make lunches",
+          id: 2,
+          completed: false
+        }
+      ];
+      todo: " "
     }
   }
 
   render() {
     return (
-      <div>
-        <h1>My Todo App!</h1>
-        {/* <div className="todo-list">
-          {this.state.todosOnState.map(todoList => (
-            <App todo={todoList} />
-          ))}
-        </div> */}
+      <div className="container">
+        <h1>My Todo List!</h1>
       </div>
     );
   }
